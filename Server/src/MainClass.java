35d2de154a09582a5f48583d5568bccce126f5de
Денис Старков < >
2016-06-16 09:31:48 +0300

    added simple Frame

--- Server/src/MainClass.java
+++ Server/src/MainClass.java
@@ -1,3 +1,4 @@
+import javax.swing.*;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
@@ -7,6 +8,10 @@ import java.util.Scanner;
 
 public class MainClass {
     public static void main(String[] args) {
+        JFrame jf = new JFrame();
+        jf.setBounds(500, 500, 100, 100);
+        jf.setTitle("Server");
+        jf.setVisible(true);
         SQLHandler.connect();
         MyServer w = new MyServer();
         //SQLHandler.disconnect();
